e0ce7d0c5e80cd48395573ffa7dc5e4fe93668df
dekramer-business <dekramer.business@gmail.com>
2021-02-21 19:34:44 -0500

    pep8 followed on comments

--- scrape_test.py
+++ scrape_test.py
@@ -12,13 +12,13 @@ class LegoSpider(scrapy.Spider):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/88.0.4324.182 Safari/537.36 '
         }
-        yield scrapy.http.Request(url, headers=headers) # Just yields once, can be used for more URL's
+        yield scrapy.http.Request(url, headers=headers)  # Just yields once, can be used for more URL's
 
     def parse(self, response):
         SET_SELECTOR = ".set"
         for brickset in response.css(SET_SELECTOR):
 
-            NAME_SELECTOR = 'h1 ::text' # Use CSS selector for getting text in h1
+            NAME_SELECTOR = 'h1 ::text'  # Use CSS selector for getting text in h1
             PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
             MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
             # IMAGE_SELECTOR = 'img ::attr(src)'
